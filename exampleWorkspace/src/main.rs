f01e8f9dca8cf57972f59a119c5bea2e1cda3072
Frederik Terstappen <freterstappen+gitlab@gmail.com>
2025-04-21 10:49:58 +0200

    chore: Add example workspace for testing

---
+++ exampleWorkspace/src/main.rs
@@ -0,0 +1,50 @@
+use bevy::{
+    self, color,
+    color::{
+        palettes,
+        palettes::{css, css::GREEN, tailwind, tailwind::PURPLE_300},
+    },
+    prelude::*,
+};
+
+fn main() {
+    let _color = bevy::color::palettes::tailwind::YELLOW_200;
+    let _color = color::palettes::tailwind::RED_500;
+    let _color = palettes::tailwind::AMBER_700;
+    let _color = tailwind::NEUTRAL_200;
+    let _color = PURPLE_300;
+
+    let _color = bevy::color::palettes::css::AZURE;
+    let _color = color::palettes::css::BLUE;
+    let _color = palettes::css::YELLOW;
+    let _color = css::RED;
+    let _color = GREEN;
+
+    let _color = Color::srgb(1.0, 0.0, 0.0);
+    let _color = Color::srgba(1.0, 0.0, 0.0, 1.0);
+
+    let _color = Color::srgba(1.0, 0.0, 0.0, 1.0);
+    let _color = Color::srgb(1.0, 1.0, 1.0);
+    let _color = Color::srgba_u8(255, 255, 255, 255);
+    let _color = Color::srgb_u8(255, 255, 255);
+    let _color = Color::linear_rgba(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::linear_rgb(1.0, 1.0, 1.0);
+    let _color = Color::hsla(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::hsl(1.0, 1.0, 1.0);
+    let _color = Color::hsva(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::hsv(1.0, 1.0, 1.0);
+    let _color = Color::hwba(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::hwb(1.0, 1.0, 1.0);
+    let _color = Color::laba(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::lab(1.0, 1.0, 1.0);
+    let _color = Color::lcha(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::lch(1.0, 1.0, 1.0);
+    let _color = Color::oklaba(1.0, 1.0, 1.0, 1.0);
+    let _color = Color::oklab(1.0, 0.0, 0.0);
+    let _color = Color::oklcha(1.0, 0.0, 0.0, 1.0);
+    let _color = Color::oklch(1.0, 0.0, 0.0);
+    let _color = Color::xyza(1.0, 0.0, 0.0, 1.0);
+    let _color = Color::xyz(1.0, 0.0, 0.0);
+
+    let _color = Color::srgb(0.58, 0.24, 0.24);
+}
